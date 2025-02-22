ce161c8f953d30cbef789053bad1ea8f230b9a2a
Andrey <Rhymes.un.limit@gmail.com>
2025-02-22 15:40:12 +0300

    Add element interface

--- TrafficLightUpdate/ViewController.swift
+++ TrafficLightUpdate/ViewController.swift
@@ -2,11 +2,16 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        redColorView.alpha = 0.5
+        yellowColorView.alpha = 0.5
+        greenColorView.alpha = 0.5
     }
-
 
 }
 
