b27e29e79d557b4db52ad3ad20c745c1543a563d
Andrey <Rhymes.un.limit@gmail.com>
2025-02-22 17:10:32 +0300

    Application logic is specified

--- TrafficLightUpdate/ViewController.swift
+++ TrafficLightUpdate/ViewController.swift
@@ -6,12 +6,40 @@ final class ViewController: UIViewController {
     @IBOutlet var yellowColorView: UIView!
     @IBOutlet var greenColorView: UIView!
     
+    @IBOutlet var startButten: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redColorView.alpha = 0.5
+        redColorView.layer.cornerRadius =
+        redColorView.frame.width / 2
+        redColorView.clipsToBounds = true
         yellowColorView.alpha = 0.5
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.width / 2
+        yellowColorView.clipsToBounds = true
         greenColorView.alpha = 0.5
+        greenColorView.layer.cornerRadius = greenColorView.frame.width / 2
+        greenColorView.clipsToBounds = true
+        
+        startButten.layer.cornerRadius = 10
     }
-
+    
+    @IBAction func startButtonDidTapped(_ sender: UIButton) {
+        if redColorView.alpha == 1 {
+            yellowColorView.alpha = 1
+            redColorView.alpha = 0.5
+        } else if yellowColorView.alpha == 1 {
+            greenColorView.alpha = 1
+            yellowColorView.alpha = 0.5
+        } else if greenColorView.alpha == 1 {
+            redColorView.alpha = 1
+            greenColorView.alpha = 0.5
+        } else {
+            redColorView.alpha = 1
+            sender.setTitle("Next", for: .normal)
+        }
+    }
+    
 }
 
