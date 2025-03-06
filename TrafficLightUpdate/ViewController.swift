aef4debfcca19816333c1edb27eea9099818b4ea
Andrey <Rhymes.un.limit@gmail.com>
2025-03-06 21:05:25 +0300

    Update code

--- TrafficLightUpdate/ViewController.swift
+++ TrafficLightUpdate/ViewController.swift
@@ -6,37 +6,52 @@ final class ViewController: UIViewController {
     @IBOutlet var yellowColorView: UIView!
     @IBOutlet var greenColorView: UIView!
     
-    @IBOutlet var startButten: UIButton!
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redColorView.alpha = 0.5
-        redColorView.layer.cornerRadius =
-        redColorView.frame.width / 2
-        yellowColorView.alpha = 0.5
+        redColorView.alpha = lightIsOff
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
+        
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
         yellowColorView.layer.cornerRadius = yellowColorView.frame.width / 2
-        greenColorView.alpha = 0.5
         greenColorView.layer.cornerRadius = greenColorView.frame.width / 2
         
-        startButten.layer.cornerRadius = 10
+        startButton.layer.cornerRadius = 10
     }
     
     @IBAction func startButtonDidTapped(_ sender: UIButton) {
-        if redColorView.alpha == 1 {
-            yellowColorView.alpha = 1
-            redColorView.alpha = 0.5
-        } else if yellowColorView.alpha == 1 {
-            greenColorView.alpha = 1
-            yellowColorView.alpha = 0.5
-        } else if greenColorView.alpha == 1 {
-            redColorView.alpha = 1
-            greenColorView.alpha = 0.5
-        } else {
-            redColorView.alpha = 1
-            sender.setTitle("Next", for: .normal)
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenColorView.alpha = lightIsOff
+            redColorView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redColorView.alpha = lightIsOff
+            yellowColorView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowColorView.alpha = lightIsOff
+            greenColorView.alpha = lightIsOn
+            currentLight = .red
         }
     }
     
 }
 
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
